816b1a83e297f105b12b4927cac2c2c19b3bd068
mo <melianiplus@gmail.com>
2024-01-04 22:13:44 +0100

    Add view-internship and review-internship Gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,16 +4,18 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-// use App\Models\Internship;
-// use App\Policies\InternshipPolicy;
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Internship;
+use App\Policies\InternshipPolicy;
+use Illuminate\Support\Facades\Gate;
 // use Illuminate\Auth\Access\Response;
-// use App\Models\User;
+use App\Models\User;
 use Spatie\Activitylog\Models\Activity;
 use App\Policies\ActivityPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    protected $administrators = ['SuperAdministrator', 'Administrator'];
+    protected $professors = ['Professor', 'HeadOfDepartment', 'ProgramCoordinator'];
     /**
      * The model to policy mappings for the application.
      *
@@ -28,7 +30,25 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('view-internship', function (User $user, Internship $internship) {
+            return $internship->student->filiere_text === $user->program_coordinator;
+        });
+        Gate::define('review-internship', [InternshipPolicy::class, 'update']);
+
+        // Gate::define('viewAny-internship', function ($user, $internship) {
+        //     // Check if the user is an administrator
+        //     if ($user->hasAnyRole($this->administrators)) {
+        //         return true;
+        //     }
+
+        //     // Check if the user is a professor and the internship's student's program matches the user's program
+        //     if ($user->hasAnyRole($this->professors) && $internship->student->filiere_text == $user->program_coordinator) {
+        //         return true;
+        //     }
+
+        //     return false;
+        // });
         $this->registerPolicies();
-        // Gate::define('review-internship', [InternshipPolicy::class, 'update']);
+
     }
 }
