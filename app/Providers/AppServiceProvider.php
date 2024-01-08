9d10c84846628bf66c2d3571b008048f18101805
mo <melianiplus@gmail.com>
2024-01-08 01:29:02 +0100

    Update AppServiceProvider to set HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
 use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use BezhanSalleh\PanelSwitch\PanelSwitch;
+use Illuminate\Support\Facades\App;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,18 +22,22 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if (App::environment('production')) {
+            $url->formatScheme('https');
+        }
         Model::unguard();
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
-                ->locales(['ar','en','fr']); // also accepts a closure
+                ->locales(['ar', 'en', 'fr']); // also accepts a closure
         });
         PanelSwitch::configureUsing(function (PanelSwitch $panelSwitch) {
             $panelSwitch
-            ->visible(fn (): bool => auth()->user()?->hasAnyRole([
-                'Administrator',
-                'SuperAdministrator',
-            ]));        });
+                ->visible(fn (): bool => auth()->user()?->hasAnyRole([
+                    'Administrator',
+                    'SuperAdministrator',
+                ]));
+        });
     }
 }
