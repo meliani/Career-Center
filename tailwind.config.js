34e9cb05d13633a957f814fbbfd3a1c94efca57d
mo <melianiplus@gmail.com>
2024-01-23 21:38:35 +0100

    Update manifest.json, package.json, and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
-
+import colors from 'tailwindcss/colors' 
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography' 
+ 
 export default {
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: { 
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            }, 
+        },
+    },
+    plugins: [
+        forms, 
+        typography, 
     ],
 }
