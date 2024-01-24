aaa6d837d793a3fded06acf02b8f869b09e46fd6
mo <melianiplus@gmail.com>
2024-01-24 15:55:32 +0100

    Add InternshipsPerCountryChart widget

---
+++ app/Filament/ProgramCoordinator/Widgets/InternshipsPerCountryChart.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\ProgramCoordinator\Widgets;
+
+use App\Filament\Core\Widgets\ApexChartsParentWidget;
+use App\Models\Internship;
+
+class InternshipsPerCountryChart extends ApexChartsParentWidget
+{
+    /**
+     * Chart Id
+     */
+    protected static ?string $chartId = 'internshipsPerCountryChart';
+
+    /**
+     * Widget Title
+     */
+    protected static ?string $heading = 'Announced Internships per Country';
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     */
+    protected function getOptions(): array
+    {
+        // get internships per Country_name
+        // $data = Internship::query()
+        //     ->select('Country_name')
+        //     ->groupBy('Country_name')
+        //     ->get();
+        //  return apex chart data
+        $internshipData = Internship::select('country', \DB::raw('count(*) as count'))
+        // where internship.student.program like AMOA
+            ->whereHas('student', function ($q) {
+
+                $q->where('program', auth()->user()->program_coordinator);
+            })
+            ->groupBy('country')
+            ->get()
+            ->toArray();
+
+        // dd(array_column($internshipData, 'country'));
+        return [
+            'chart' => [
+                'type' => 'pie',
+                'height' => 300,
+            ],
+            'series' => array_column($internshipData, 'count'),
+            'labels' => array_column($internshipData, 'country'),
+            'legend' => [
+                'labels' => [
+                    'fontFamily' => 'inherit',
+                ],
+            ],
+        ];
+    }
+}
