1ed869e602ab734793c0377a5b5cdabda9509a21
mo <melianiplus@gmail.com>
2024-02-02 21:02:14 +0100

    Refactor department and program enums

--- app/Enums/Department.php
+++ app/Enums/Department.php
@@ -6,33 +6,24 @@ use Filament\Support\Contracts\HasLabel;
 
 enum Department: string implements HasLabel
 {
-    // case AMOA = 'Ingénieur Innovation et AMOA';
-    // case ASEDS = 'Advanced Software Engineering for Digital Services';
-    // case DATA = 'Ingénieur des Sciences de Données';
-    // case ICCN = 'Ingénieur Cybersécurité Et Confiance Numérique';
-    // case SESNUM = 'Systèmes Embraqués et Services Numériques';
-    // case SMARTICT = 'Ingénierie Smart  ICT« Smart Information & Communication Technology Engineering';
-    // case SUD = 'Ingénierie des Systèmes Ubiquitaires et Distribués – Cloud et IoT (SUD)';
-    case AMOA = 'AMOA';
-    case ASEDS = 'ASEDS';
-    case DATA = 'DATA';
-    case ICCN = 'ICCN';
-    case SESNUM = 'SESNUM';
-    case SMARTICT = 'SMART-ICT';
-    case SUD = 'SUD';
+    /* MIR,     Département Systèmes de Communications.
+    Département Eélectronique , Micro ondes et Optique.
+    Département Mathématiques, Informatique et Réseaux.
+    Département Gestion, Langues et Communications. */
+    case EMO = 'EMO';
+    case MIR = 'MIR';
+    case GLC = 'GLC';
+    case SC = 'SC';
 
 
 
     public function getLabel(): ?string
     {
         return match ($this) {
-            self::AMOA => 'Ingénieur Innovation et AMOA',
-            self::ASEDS => 'Advanced Software Engineering for Digital Services',
-            self::DATA => 'Ingénieur des Sciences de Données',
-            self::ICCN => 'Ingénieur Cybersécurité Et Confiance Numérique',
-            self::SESNUM => 'Systèmes Embraqués et Services Numériques',
-            self::SMARTICT => 'Ingénierie Smart  ICT« Smart Information & Communication Technology Engineering',
-            self::SUD => 'Ingénierie des Systèmes Ubiquitaires et Distribués – Cloud et IoT (SUD)',
+            self::EMO => 'Département Électronique, Micro-ondes et Optique',
+            self::MIR => 'Département Systèmes de Communications',
+            self::GLC => 'Département Gestion, Langues et Communications',
+            self::SC => 'Département Mathématiques, Informatique et Réseaux',
         };
     }
 }
