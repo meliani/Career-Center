0c6aa2200ed7136114dff469f4785463e29fefea
mo <melianiplus@gmail.com>
2024-02-03 15:37:24 +0100

    Add new fillable and cast properties to Student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -19,6 +19,29 @@ class Student extends Person implements HasMedia
     protected $appends = [
         'full_name',
     ];
+    protected $fillable = [
+        'title',
+        'pin',
+        'first_name',
+        'last_name',
+        'email_perso',
+        'phone',
+        'cv',
+        'lm',
+        'photo',
+        'birth_date',
+        'current_year',
+        'program',
+        'is_mobility',
+        'abroad_school',
+        'year_id',
+        'is_active',
+        'model_status_id',
+        'graduated_at',
+        'parrain_titre',
+        'encadrant_ext_titre',
+        'encadrant_int_titre',
+    ];
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
@@ -28,6 +51,8 @@ class Student extends Person implements HasMedia
         'program' => Enums\Program::class,
         'parrain_titre' => Enums\Title::class,
         'encadrant_ext_titre' => Enums\Title::class,
+        'encadrant_int_titre' => Enums\Title::class,
+        'current_year' => Enums\CurrentYear::class,
 
     ];
     public static function boot()
