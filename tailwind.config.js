06c3336fed6a9c97f70ca8cbf00d85077a180172
mo <melianiplus@gmail.com>
2024-03-13 06:20:54 +0000

    Update tailwind.config.js and add tailwind.config.preset.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
-import preset from './vendor/filament/support/tailwind.config.preset'
+// import preset from './vendor/filament/support/tailwind.config.preset'
 import colors from 'tailwindcss/colors' 
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography' 
- 
+
+import preset from './tailwind.config.preset'
+
+
 export default {
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        // './app/**/*.php',
+        // "./resources/**/*.{php,html,js,jsx,ts,tsx,vue,twig}",
     ],
     darkMode: 'class',
     theme: {
@@ -20,6 +25,11 @@ export default {
                 warning: colors.yellow,
             }, 
         },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+        }
     },
     plugins: [
         forms, 
