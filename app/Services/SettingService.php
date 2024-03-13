6d5035d99ccd3f1c23a728f18ee6ec83b170d955
mo <melianiplus@gmail.com>
2024-03-13 00:46:46 +0000

    Add .env.example and SettingService.php files

---
+++ app/Services/SettingService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+class SettingService
+{
+    // protected static $in_app = app(\App\Settings\NotificationSettings::class)->in_app;
+
+    // protected static $by_email = app(\App\Settings\NotificationSettings::class)->by_email;
+
+    // protected static $by_sms = app(\App\Settings\NotificationSettings::class)->by_sms;
+
+    // public static function getInApp()
+    // {
+    //     return self::$in_app;
+    // }
+
+    // public function getByEmail()
+    // {
+    //     return self::$by_email;
+    // }
+
+    // public function getBySms()
+    // {
+    //     return self::$by_sms;
+    // }
+}
