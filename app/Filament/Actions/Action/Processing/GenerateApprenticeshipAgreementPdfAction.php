00999bea42cca6a28fd2f6ad1b8b961da5dc57fa
mo <melianiplus@gmail.com>
2024-04-26 15:36:45 +0100

    Refactor GenerateApprenticeshipAgreementPdfAction.php to remove commented out Browsershot configuration

--- app/Filament/Actions/Action/Processing/GenerateApprenticeshipAgreementPdfAction.php
+++ app/Filament/Actions/Action/Processing/GenerateApprenticeshipAgreementPdfAction.php
@@ -33,11 +33,11 @@ class GenerateApprenticeshipAgreementPdfAction extends Action
 
             $chromePath = env('BROWSERSHOT_CHROME_PATH');
             pdf()
-                ->withBrowsershot(function (Browsershot $browsershot) use ($chromePath) {
-                    $browsershot
-                        // ->noSandbox()
-                        ->setChromePath($chromePath);
-                })
+                // ->withBrowsershot(function (Browsershot $browsershot) use ($chromePath) {
+                //     $browsershot
+                //         // ->noSandbox()
+                //         ->setChromePath($chromePath);
+                // })
                 ->view($template_view, ['internship' => $apprenticeship])
                     // ->name('InternshipAgreement.pdf')
                     // ->withBrowsershot(function (Browsershot $browsershot) {
