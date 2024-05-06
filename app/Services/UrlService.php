403f588bfca8ccbb0aa35ee010ceab16bcd13efb
mo <melianiplus@gmail.com>
2024-05-06 19:33:07 +0100

    chore: Add QR code generation to UrlService

--- app/Services/UrlService.php
+++ app/Services/UrlService.php
@@ -3,6 +3,12 @@
 namespace App\Services;
 
 use App\Enums;
+use BaconQrCode\Renderer\Color\Rgb;
+use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\Fill;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
 use Illuminate\Support\Facades\Crypt;
 
 class UrlService
@@ -73,5 +79,17 @@ class UrlService
     public static function decodeUrl($url)
     {
         return self::decryptv1(self::decapsulate($url));
+    }
+
+    public function getQrCodeSvg(string $url)
+    {
+        $svg = (new Writer(
+            new ImageRenderer(
+                new RendererStyle(150, 1, null, null, Fill::uniformColor(new Rgb(255, 255, 255), new Rgb(45, 55, 72))),
+                new SvgImageBackEnd()
+            )
+        ))->writeString($url);
+
+        return trim(substr($svg, strpos($svg, "\n") + 1));
     }
 }
