85fed664e1189adc57d8ef18539726e1ea2fb835
mo <melianiplus@gmail.com>
2024-05-15 17:56:31 +0100

    Update password reset translations in English and French

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -3,4 +3,5 @@
 return [
     'sent' => 'We have emailed your password reset link!',
     'user' => "We can't find a user with that email address.",
+    'reset' => 'Your password has been reset!',
 ];
--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -3,4 +3,5 @@
 return [
     'sent' => 'Nous avons envoyé votre lien de réinitialisation de mot de passe par e-mail!',
     'user' => 'Nous ne pouvons pas trouver un utilisateur avec cette adresse e-mail.',
+    'reset' => 'Votre mot de passe a été réinitialisé!',
 ];
