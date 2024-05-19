ca09b46624b57bb0bd0b8129f393b07cd10fe86a
mo <melianiplus@gmail.com>
2024-05-19 22:24:19 +0100

    chore: Update GenerateTimeslotsFromArtisanAction to use schedule_starting_at and schedule_ending_at

--- app/Filament/Actions/Action/Processing/GenerateTimeslotsFromArtisanAction.php
+++ app/Filament/Actions/Action/Processing/GenerateTimeslotsFromArtisanAction.php
@@ -2,13 +2,8 @@
 
 namespace App\Filament\Actions\Action\Processing;
 
-use App\Models\ScheduleParameters;
-use App\Models\Timeslot;
-use App\Services\AutoScheduleService;
 // use App\Services\TimeslotService;
 use Filament\Forms\Components\Actions\Action;
-use Filament\Notifications\Notification;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
 
 class GenerateTimeslotsFromArtisanAction extends Action
@@ -22,16 +17,14 @@ class GenerateTimeslotsFromArtisanAction extends Action
         ]);
 
         $static->configure()
-        ->action(function ($record): void {
-            Artisan::call('app:generate-timeslots', [
-                //     protected $signature = 'app:generate-timeslots {--start-date=2024-06-24} {--end-date=2024-07-24}';
-                '--start-date' => $record->start_date,
-                '--end-date' => $record->end_date,
-            ]);
-        });
+            ->action(function ($record): void {
+                Artisan::call('app:generate-timeslots', [
+                    //     protected $signature = 'app:generate-timeslots {--start-date=2024-06-24} {--end-date=2024-07-24}';
+                    '--start-date' => $record->schedule_starting_at,
+                    '--end-date' => $record->schedule_ending_at,
+                ]);
+            });
 
         return $static;
     }
-
-
 }
