0464e3386cf5e42abc3ed3cd1cd33a5584434cda
mo <melianiplus@gmail.com>
2024-06-27 17:13:11 +0100

    `Refactor organization selection in apprenticeship agreement form`

--- app/Filament/App/Resources/ApprenticeshipResource/Pages/CreateApprenticeship.php
+++ app/Filament/App/Resources/ApprenticeshipResource/Pages/CreateApprenticeship.php
@@ -3,10 +3,29 @@
 namespace App\Filament\App\Resources\ApprenticeshipResource\Pages;
 
 use App\Filament\App\Resources\ApprenticeshipResource;
-use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateApprenticeship extends CreateRecord
 {
     protected static string $resource = ApprenticeshipResource::class;
+
+    protected function getCreateFormAction(): Action
+    {
+        return Action::make('create')
+            ->label(__('filament-panels::resources/pages/create-record.form.actions.create.label'))
+            ->requiresConfirmation()
+            ->modalHeading(__('Notice'))
+            ->modalDescription(__('The residence will not be available after July 31st. Please make the necessary arrangements.'))
+            ->modalSubmitActionLabel(__('I understand'))
+            ->color('success')
+
+            ->action(fn () => $this->create())
+            ->keyBindings(['mod+s']);
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
