e7d8c39fc2df5f669f96eb697398c02c06ff081f
mo <melianiplus@gmail.com>
2024-06-30 17:37:33 +0100

    Added preview email and steps to send an email `Refactor language files to add missing translations and update existing ones`

--- app/Filament/Actions/Action/SendDefenseEmailAction.php
+++ app/Filament/Actions/Action/SendDefenseEmailAction.php
@@ -4,7 +4,9 @@ namespace App\Filament\Actions\Action;
 
 use App\Models\Project;
 use App\Notifications\DefenseAuthorizedNotification;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Tables\Actions\Action;
+use Illuminate\Support\HtmlString;
 
 class SendDefenseEmailAction extends Action
 {
@@ -24,30 +26,87 @@ class SendDefenseEmailAction extends Action
             'name' => $name ?? static::getDefaultName(),
         ]);
         $static->configure()->action(function (array $data, Project $record): void {
-            // event(new \App\Events\DefenseAuthorized($record, $data['emails']));
+            event(new \App\Events\DefenseAuthorized($record, $data['emails']));
             // dd($notification->toMail(auth()->user()->email)->render());
 
-        })->form(function ($record) {
-            $notification = new DefenseAuthorizedNotification($record);
-
-            self::$emailBody = $notification->toMail(auth()->user()->email)->render();
-
-            return [
-                \Filament\Forms\Components\Textarea::make('email_body')
-                    ->label('Email Body')
-                    ->default(function ($record) {}),
-                \Filament\Forms\Components\TagsInput::make('emails')
-                    ->label('Emails')
-                    ->placeholder(__('Enter emails separated by commas'))
-                    ->default(function () use ($record) {
-                        // Assuming getEmails is a method that takes the $record and returns an array of emails
-                        return $record ? self::getEmails($record) : [];
-                    }),
-                // Uncomment and adjust the rules as necessary
-                // ->rules('required', 'email', 'max:255'),
-            ];
         })
-            ->requiresConfirmation();
+            // ->fillForm(function ($record) {
+            //     $notification = new DefenseAuthorizedNotification($record);
+
+            //     self::$emailBody = $notification->toMail(auth()->user()->email)->render();
+            //     // dd(self::$emailBody);
+
+            //     return [
+            //         'email_body' => fn () => new HtmlString(self::$emailBody),
+            //         // 'email_body' => self::$emailBody,
+            //         // 'emails' => self::getEmails($record),
+            //     ];
+            // })
+            ->steps(function ($record) {
+                $notification = new DefenseAuthorizedNotification($record);
+
+                self::$emailBody = $notification->toMail(auth()->user()->email)->render();
+
+                return [
+                    Step::make('Preview')
+                        ->label(__('Preview'))
+                        ->description(__('Preview email'))
+                        ->schema([
+                            // \Filament\Forms\Components\RichEditor::make('Preview1')
+                            //     ->columnSpanFull()
+                            //     ->default(new HtmlString(self::$emailBody)),
+                            \Filament\Forms\Components\Placeholder::make('Preview')->hiddenLabel()
+                                ->content(self::$emailBody),
+                        ]),
+                    Step::make('Recipients')
+                        ->label(__('Recipients'))
+                        ->description(__('View and edit recipients'))
+                        ->schema([
+
+                            \Filament\Forms\Components\TagsInput::make('emails')
+                                ->label('Emails')
+                                ->splitKeys(['Tab', ',', ';', ' '])
+                                ->nestedRecursiveRules([
+                                    'email',
+                                ])
+                                ->placeholder(__('Enter emails separated by commas'))
+                                ->default(function () use ($record) {
+                                    // Assuming getEmails is a method that takes the $record and returns an array of emails
+                                    return $record ? self::getEmails($record) : [];
+                                }),
+                        ]),
+
+                ];
+            });
+        // ->form(function ($record) {
+        //     $notification = new DefenseAuthorizedNotification($record);
+
+        //     self::$emailBody = $notification->toMail(auth()->user()->email)->render();
+
+        //     return [
+        //         // \Filament\Forms\Components\MarkdownEditor::make('email_body')
+        //         //     ->label('Email Body'),
+        //         \Filament\Forms\Components\Placeholder::make('Preview')->hiddenLabel()
+        //             ->content(self::$emailBody),
+        //         // Forms\Components\RichEditor::make('description')
+        //         //     ->columnSpanFull(),
+        //         \Filament\Forms\Components\TagsInput::make('emails')
+        //             ->label('Emails')
+        //             ->splitKeys(['Tab', ',', ';'])
+        //             ->nestedRecursiveRules([
+        //                 'min:2',
+        //                 'max:50',
+        //             ])
+        //             ->placeholder(__('Enter emails separated by commas'))
+        //             ->default(function () use ($record) {
+        //                 // Assuming getEmails is a method that takes the $record and returns an array of emails
+        //                 return $record ? self::getEmails($record) : [];
+        //             }),
+        //         // Uncomment and adjust the rules as necessary
+        //         // ->rules('required', 'email', 'max:255'),
+        //     ];
+        // })
+        // ->color('success');
 
         return $static;
     }
