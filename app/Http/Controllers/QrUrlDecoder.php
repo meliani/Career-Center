b8133f259eab778582dbecbdbeac6b569bc6ed26
mo <melianiplus@gmail.com>
2024-07-11 12:49:23 +0100

    `Refactor code to update QR URL decoding logic in QrUrlDecoder`

--- app/Http/Controllers/QrUrlDecoder.php
+++ app/Http/Controllers/QrUrlDecoder.php
@@ -2,16 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Apprenticeship;
+use App\Models\Student;
+use App\Services\UrlService;
 use Illuminate\Http\Request;
 
 class QrUrlDecoder extends Controller
 {
     public function __invoke(Request $request)
     {
-        /* Route::get(
-        '/verify-diploma/{verification_code}',
-        DiplomaVerificationController::class
-        ) */
+        $x = $request->get('x');
+        $InternshipHash = UrlService::decodeUrl($x);
 
+        // Exemple pour afficher les informations de l'étudiant et du stage
+        $studentId = $InternshipHash['StudentId'];
+        $internshipId = $InternshipHash['InternshipId'];
+
+        if (is_null($studentId) || is_null($internshipId)) {
+            return view('filament.org.pages.qr-response', [
+                'slot' => 'qr-response',
+                'studentId' => null,
+                'internshipId' => null,
+                'is_authentic' => false,
+            ]);
+        }
+
+        $student = Student::find($studentId);
+        $internship = Apprenticeship::find($internshipId);
+
+        return view('filament.org.pages.qr-response', [
+            'slot' => 'qr-response',
+            'student' => $student,
+            'internship' => $internship,
+            'is_authentic' => true,
+        ]);
+        // ->layout('components.layouts.public');
     }
 }
