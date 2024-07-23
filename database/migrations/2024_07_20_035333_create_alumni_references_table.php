d9297b53443f89a5d0b2f017d59bb93c562a63e0
mo <melianiplus@gmail.com>
2024-07-23 20:32:02 +0100

    `Refactor code to update access control in AlumniDegree enum`

--- database/migrations/2024_07_20_035333_create_alumni_references_table.php
+++ database/migrations/2024_07_20_035333_create_alumni_references_table.php
@@ -2,6 +2,7 @@
 
 use App\Enums\AlumniDegree;
 use App\Enums\Program;
+use App\Enums\Title;
 use App\Enums\WorkStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,7 +17,7 @@ return new class extends Migration
     {
         Schema::create('alumni_references', function (Blueprint $table) {
             $table->id();
-            $table->enum('title', ['Mr', 'Mrs'])->nullable();
+            $table->enum('title', array_map(fn ($case) => $case->name, Title::cases()))->nullable();
             $table->string('name')->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
