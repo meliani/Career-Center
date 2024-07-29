7a0ee90102f24c8b9596af11654a7412e618e00f
mo <melianiplus@gmail.com>
2024-07-30 00:50:06 +0100

    `Refactor AlumniReference model to include additional fields and update table name`

--- app/Models/AlumniReference.php
+++ app/Models/AlumniReference.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class AlumniReference extends Model
 {
     use HasFactory;
+
+    protected $table = 'alumni_references';
+
+    protected $fillable = [
+        'title',
+        'name',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'graduation_year_id',
+        'degree',
+        'program',
+        'is_enabled',
+        'is_mobility',
+        'abroad_school',
+        'work_status',
+        'resume_url',
+        'avatar_url',
+        'number_of_bounces',
+        'bounce_reason',
+    ];
+
+    protected $casts = [
+        'is_enabled' => 'boolean',
+        'is_mobility' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // protected $dates = [
+    //     'created_at',
+    //     'updated_at',
+    // ];
 }
