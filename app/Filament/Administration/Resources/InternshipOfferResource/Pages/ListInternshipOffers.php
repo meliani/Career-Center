cf2c31b471d54b645a46c7ce4074980001af65db
mo <melianiplus@gmail.com>
2024-10-13 21:25:03 +0100

    feat: Add internship level tabs with offer counts to ListInternshipOffers page

--- app/Filament/Administration/Resources/InternshipOfferResource/Pages/ListInternshipOffers.php
+++ app/Filament/Administration/Resources/InternshipOfferResource/Pages/ListInternshipOffers.php
@@ -2,18 +2,50 @@
 
 namespace App\Filament\Administration\Resources\InternshipOfferResource\Pages;
 
+use App\Enums\InternshipLevel;
 use App\Filament\Administration\Resources\InternshipOfferResource;
+use App\Models\InternshipOffer;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListInternshipOffers extends ListRecords
 {
     protected static string $resource = InternshipOfferResource::class;
 
+    public array $InternshipOffersByInternshipLevel = [];
+
+    public function mount(): void
+    {
+        // Populate the property with the count of internship offers for each level
+        $this->InternshipOffersByInternshipLevel = InternshipOffer::query()
+            ->selectRaw('internship_level, COUNT(*) as count')
+            ->groupBy('internship_level')
+            ->pluck('count', 'internship_level')
+            ->toArray();
+    }
+
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        // tabs by internship level
+        $tabs = [];
+
+        $tabs['all'] = Tab::make('All');
+
+        foreach (InternshipLevel::getArray() as $level) {
+            $levelLabel = $level->value; // Convert enum instance to string
+            $tabs[$levelLabel] = Tab::make($level->getLabel())
+                ->badge($this->InternshipOffersByInternshipLevel[$levelLabel] ?? 0)
+                ->modifyQueryUsing(fn ($query) => $query->where('internship_level', $levelLabel));
+        }
+
+        return $tabs;
     }
 }
