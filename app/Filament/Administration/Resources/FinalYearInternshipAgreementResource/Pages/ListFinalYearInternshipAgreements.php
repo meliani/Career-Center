06b0e4e7961c6f6ed3a5ba8245682932ab9bc65b
mo <melianiplus@gmail.com>
2024-11-11 04:02:39 +0100

    feat: Add tabbed interface for FinalYearInternshipAgreements with status-based filtering and counts

--- app/Filament/Administration/Resources/FinalYearInternshipAgreementResource/Pages/ListFinalYearInternshipAgreements.php
+++ app/Filament/Administration/Resources/FinalYearInternshipAgreementResource/Pages/ListFinalYearInternshipAgreements.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Administration\Resources\FinalYearInternshipAgreementResource\Pages;
 
+use App\Enums;
 use App\Filament\Administration\Resources\FinalYearInternshipAgreementResource;
+use App\Models\FinalYearInternshipAgreement;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListFinalYearInternshipAgreements extends ListRecords
 {
@@ -14,6 +18,28 @@ class ListFinalYearInternshipAgreements extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            __('All') => Tab::make(),
+            __('Draft') => Tab::make()
+                ->badge(FinalYearInternshipAgreement::where('status', Enums\Status::Draft->value)->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', Enums\Status::Draft->value)),
+            __('Announced') => Tab::make()
+                ->badge(FinalYearInternshipAgreement::where('status', Enums\Status::Announced->value)->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', Enums\Status::Announced->value)),
+            __('Validated') => Tab::make()
+                ->badge(FinalYearInternshipAgreement::where('status', Enums\Status::Validated->value)->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', Enums\Status::Validated->value)),
+            __('Completed') => Tab::make()
+                ->badge(FinalYearInternshipAgreement::where('status', Enums\Status::Completed->value)->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', Enums\Status::Completed->value)),
+            __('Signed') => Tab::make()
+                ->badge(FinalYearInternshipAgreement::where('status', Enums\Status::Signed->value)->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', Enums\Status::Signed->value)),
         ];
     }
 }
