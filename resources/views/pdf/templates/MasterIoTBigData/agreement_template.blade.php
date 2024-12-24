51e79ea447335fb8a3eab7b9271c3313296e945e
mo <melianiplus@gmail.com>
2024-12-24 21:29:52 +0100

    feat: Adjust minimum height for signature sections in Master IoT and Big Data agreement template

--- resources/views/pdf/templates/MasterIoTBigData/agreement_template.blade.php
+++ resources/views/pdf/templates/MasterIoTBigData/agreement_template.blade.php
@@ -782,13 +782,13 @@
             <strong>Document établi en quatre exemplaires</strong>
         </div>
         <div class="flex flex-row justify-between items-center mt-4 space-x-4 text-xs text-center mb-auto">
-            <div class="border-2 border-gray-800 w-full p-0 flex flex-col items-center min-h-[250px]">
+            <div class="border-2 border-gray-800 w-full p-0 flex flex-col items-center min-h-[230px]">
                 <div class="min-h-[100px] px-2 pt-2 text-gray-900">Date et signature du Chef d’Entreprise (Cachet de
                     l’Entreprise)
                 </div>
                 {{-- <div class="border-t-2 pt-2 text-gray-400 text-center">Cachet de l’organisme d'accueil</div> --}}
             </div>
-            <div class="border-2 border-gray-800 w-full p-0 flex flex-col items-center min-h-[250px]">
+            <div class="border-2 border-gray-800 w-full p-0 flex flex-col items-center min-h-[230px]">
                 <div class="min-h-[100px] text-gray-900 px-2 pt-2">Date et Signature du stagiaire avec Mention
                     manuscrite «
                     Lu et
@@ -796,7 +796,7 @@
                 {{-- <div class="border-t-2 pt-2 text-gray-400 text-center">Mention manuscrite « Lu et approuvé »</div>
                 --}}
             </div>
-            <div class="border-2 border-gray-900 w-full pt-0 flex flex-col justify-between min-h-[250px]">
+            <div class="border-2 border-gray-900 w-full pt-0 flex flex-col justify-between min-h-[230px]">
                 <div class="px-2 pt-2 text-gray-800 text-center">Le Directeur de l’INPT (ou son Représentant)</div>
             </div>
         </div>
