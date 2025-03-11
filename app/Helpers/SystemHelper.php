f2218145fa9a5fa6dfc4dfa0c67bfcd509576062
mo <melianiplus@gmail.com>
2025-03-11 01:32:29 +0000

    Remove placeholder comment from systemUser function in SystemHelper

--- app/Helpers/SystemHelper.php
+++ app/Helpers/SystemHelper.php
@@ -5,7 +5,6 @@
 if (! function_exists('systemUser')) {
     function systemUser()
     {
-        // Replace this with the actual logic to get the system user
         return \App\Models\User::where('role', 'System')->first();
     }
 }
