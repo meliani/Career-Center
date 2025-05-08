72caef6624e2ef2ed3ab01092afbf0c4ecfb491c
mo <melianiplus@gmail.com>
2025-05-08 12:11:36 +0100

    Add watermark styling to internship agreement templates

--- resources/views/pdf/templates/SecondYear/agreement_template.blade.php
+++ resources/views/pdf/templates/SecondYear/agreement_template.blade.php
@@ -2,8 +2,22 @@
 
 <head>
     <title>Internship Agreement</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-
+    <style>
+        {!! file_get_contents(public_path('build/assets/pdf.css')) !!}
+        @if(!empty($watermark))
+        body::after {
+            content: "{{ $watermark }}";
+            position: fixed;
+            top: 40%;
+            left: 30%;
+            font-size: 10em;
+            color: rgba(0,0,0,0.1);
+            transform: rotate(-30deg);
+            pointer-events: none;
+            z-index: 9999;
+        }
+        @endif
+    </style>
 </head>
 
 <body class="text-justify text-xs sm:text-sm md:text-base lg:text-lg">
