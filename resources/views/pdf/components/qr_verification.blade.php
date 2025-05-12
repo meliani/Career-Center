028db245d4dac372751e81cc2cea634963c7d4db
mo <melianiplus@gmail.com>
2025-05-12 05:09:56 +0100

    fix: Adjust margins and update text for QR code verification and title box components

--- resources/views/pdf/components/qr_verification.blade.php
+++ resources/views/pdf/components/qr_verification.blade.php
@@ -1,8 +1,6 @@
 {{-- QR Code verification component --}}
-<div class="flex justify-center items-center mt-20">
+<div class="flex justify-center items-center mt-0">
     {!! $qrCodeSvg !!}
 </div>
 <div class="text-center text-[0.60rem] italic mb-2">
-    <p>Ce tag est pour la vérification</p>
-    <p>de l'authenticité de ce document</p>
-</div>
+    <p>Ce QR code permet de vérifier l'authenticité de ce document.</p>
