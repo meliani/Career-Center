011db325265e379b877f7624f57247604a38f2e3
mel <m@meliani.xyz>
2025-05-21 00:20:39 +0100

    feat: Enhance ManageDisplay page with navigation label, group, and access control methods

--- app/Filament/Administration/Pages/ManageDisplay.php
+++ app/Filament/Administration/Pages/ManageDisplay.php
@@ -9,9 +9,40 @@ use Filament\Pages\SettingsPage;
 
 class ManageDisplay extends SettingsPage
 {
-    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+        protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
 
     protected static string $settings = DisplaySettings::class;
+
+    protected static ?string $title = 'Display Settings';
+
+    protected static ?string $navigationLabel = 'Display Settings';
+
+    protected static ?string $navigationGroup = 'Settings';
+
+    public static function getNavigationLabel(): string
+    {
+        return __(self::$navigationLabel);
+    }
+
+    public static function getnavigationGroup(): string
+    {
+        return __(self::$navigationGroup);
+    }
+
+    public function getTitle(): string
+    {
+        return __(self::$title);
+    }
+
+    public static function canAccess(): bool
+    {
+        if (auth()->check()) {
+            return auth()->user()->isSuperAdministrator() || auth()->user()->isAdministrator();
+        } else {
+            return false;
+        }
+    }
+
 
     public function form(Form $form): Form
     {
