12778bf0774a4693dce72dcee3493889d3fb26c0
mel <m@meliani.xyz>
2025-06-06 00:58:24 +0100

    feat: Add French translation for "No filters active" in project listing page

--- resources/views/filament/administration/resources/project-resource/pages/list-projects.blade.php
+++ resources/views/filament/administration/resources/project-resource/pages/list-projects.blade.php
@@ -60,7 +60,7 @@
                             <svg class="h-3 w-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                             </svg>
-                            No filters active
+                            {{__('No filters active')}}
                         </span>
                     @endif
                 </div>
