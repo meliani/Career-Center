c6c14e78ec7a0fd84d1ad0c7592558dcdc5587e8
mel <m@meliani.xyz>
2025-06-05 16:11:20 +0100

    feat: Enhance Defense Authorized Notification email with additional project and supervisor information

--- app/Notifications/DefenseAuthorizedNotification.php
+++ app/Notifications/DefenseAuthorizedNotification.php
@@ -29,10 +29,17 @@ class DefenseAuthorizedNotification extends Notification
             ->template('emails.templates.notification_email')
             ->from(auth()->user()->email, auth()->user()->full_name)
             ->subject(__('The Defense :id has been authorized', ['id' => $this->project->id_pfe]))
+            
+            // Main heading
             ->line(__('# The Defense :id has been authorized.', ['id' => $this->project->id_pfe]))
-            // Include evaluation sheet data using ->line()
+            ->line('---')
+            
+            // Defense overview
             ->line(__('**Administrative supervisor:** ') . $this->project->administrative_supervisor)
             ->line(__('**Defense Date:** ') . $this->project->defense_plan)
+            ->line('---')
+            
+            // Student information section
             ->line(__('## Student information'))
             ->line(__('**Name:** ') . ($this->project->students ? $this->project->students->implode('full_name', ' & ') : 'Undefined'))
             ->line(__('**ID PFE:** ') . $this->project->id_pfe)
@@ -40,22 +47,40 @@ class DefenseAuthorizedNotification extends Notification
                 return $student->program->value;
             })->implode(' & ') : 'Undefined'))
             ->line(__('**Organization:** ') . $this->project->organization_name)
+            ->line('---')
+            
+            // Project information section
             ->line(__('## Project information'))
             ->line(__('**Title:** ') . $this->project->title)
+            ->line('---')
+            
+            // Supervisors section
             ->line(__('## Supervisors'))
             ->line(__('**Academic supervisor:** ') . $this->project->academic_supervisor_name)
             ->line(__('**External supervisor:** ') . $this->project->external_supervisor_name)
+            ->line('---')
+            
+            // Reviewers section
             ->line(__('## Reviewers'))
             ->line(__('**Reviewer 1:** ') . $this->project->reviewer1)
             ->line(__('**Reviewer 2:** ') . $this->project->reviewer2)
+            ->line('---')
+            
+            // Organization evaluation sheet (conditional)
             ->lineIf($this->project->organization_evaluation_sheet_url, __('## Organization Evaluation sheet'))
             ->lineIf($this->project->organization_evaluation_sheet_url, __('You can download the organization evaluation sheet from the link below.'))
             ->lineIf($this->project->organization_evaluation_sheet_url, __('[Download Organization Evaluation Sheet](:url)', ['url' => $this->project->organization_evaluation_sheet_url]))
+            ->lineIf($this->project->organization_evaluation_sheet_url, '---')
+            
+            // Jury evaluation sheet section
             ->line(__('## Jury Evaluation sheet'))
             ->line(__('You can download the evaluation sheet from the link below.'))
             ->line(__('[Download Evaluation Sheet](:url)', ['url' => $this->project->evaluation_sheet_url]))
-
+            ->line('---')
+            
+            // Action button and closing
             ->action(__('View Planning'), url('https://carrieres.inpt.ac.ma/soutenances'))
+            ->line('---')
             ->line(__('Regards'))
             ->salutation(auth()->user()->full_name . ' (' . auth()->user()->email . ')');
         // ->attach($this->project->evaluation_sheet_url, [
